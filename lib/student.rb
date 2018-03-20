49895df612a5a9cf79e7fe1f2222f7db10121c99
JD Leshan <stritch@gmail.com>
2018-03-20 16:50:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,7 @@
 require_relative 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  def initialize(options={})
-    super
-    Student.column_names.each do |column_name|
+    self.column_names.each do |column_name|
       self.attr_accessor column_name.to_sym
     end
-  end
 end
