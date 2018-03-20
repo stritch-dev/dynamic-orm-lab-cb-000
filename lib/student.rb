0f24133ebcfff20cd28cc638feba5ff4eb91d720
JD Leshan <stritch@gmail.com>
2018-03-20 16:55:12 +0000

    removed 'Student' class reference for attr_accessor

--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,9 @@
-require_relative 'interactive_record.rb'
+require_relative "../config/environment.rb"
+require 'active_support/inflector'
+require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  def initialize(options={})
-    super
-    Student.column_names.each do |column_name|
-      Student.attr_accessor column_name.to_sym
-    end
+  Student.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
   end
 end
